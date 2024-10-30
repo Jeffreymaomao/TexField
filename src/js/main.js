0d6b5d9b1c82283e96d7c90ead820fe836d93545
Chang Mao Yang <jeffrey0613mao@gmail.com>
2024-10-30 16:59:02 +0800

    link icon bar

--- src/js/main.js
+++ src/js/main.js
@@ -4,9 +4,27 @@ import '../css/matheditor.css';
 import '../css/style.css';
 
 import TexField from './TexField.js';
+import {createAndAppendElement} from './Tool.js';
+
 
 window.addEventListener("load", (e)=>{
 	const app = new TexField({});
-    app.addNote(window.innerWidth*0.5, window.innerHeight*0.5);
-	window.app = app
+	app.addNote(window.innerWidth*0.5, window.innerHeight*0.5);
+	// window.app = app;
+	const linkContainer = createAndAppendElement(app.dom.container, 'div', {
+		class: 'link-container'
+	});
+	const github = createAndAppendElement(linkContainer, 'a', {
+		class: 'link-github',
+		href: 'https://github.com/Jeffreymaomao/TexField',
+		target: '_blank',
+		draggable: false,
+		title: 'GitHub'
+	});
+	const help = createAndAppendElement(linkContainer, 'a', {
+		class: 'link-help',
+		href: '#',
+		draggable: false,
+		title: 'Help'
+	});
 });
