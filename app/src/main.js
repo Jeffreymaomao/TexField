9d234bd16addad13fd5ea3c05c14e7c623b70101
Chang Mao Yang <jeffrey0613mao@gmail.com>
2024-11-01 17:37:21 +0800

    help and github

--- app/src/main.js
+++ app/src/main.js
@@ -1,9 +1,50 @@
 import TexField from '../../src/js/TexField.js';
 import askOllama from './plugin.ai.js';
 
-window.addEventListener("load", (e) => {
+import {createAndAppendElement} from '../../src/js/Tool.js';
+
+const visited = localStorage.getItem('visited');
+window.addEventListener("load", (e)=>{
+    document.querySelector('h1').innerText = 'TexField';
+    // ---
     const app = new TexField({});
-    app.addNote(window.innerWidth * 0.5, window.innerHeight * 0.5);
-    window.app = app;
     app.askChatGPT = ()=>{askOllama(app)}
+    app.addNote(window.innerWidth*0.5, window.innerHeight*0.5);
+    // window.app = app;
+    const linkContainer = createAndAppendElement(app.dom.container, 'div', {
+        class: 'link-container'
+    });
+    const github = createAndAppendElement(linkContainer, 'a', {
+        class: 'link-github',
+        href: 'https://github.com/Jeffreymaomao/TexField',
+        target: '_blank',
+        draggable: false,
+        title: 'GitHub'
+    });
+    const help = createAndAppendElement(linkContainer, 'a', {
+        class: 'link-help',
+        href: '#',
+        draggable: false,
+        title: 'Help'
+    });
+    // ---
+    const helpContainer = document.querySelector('#help');
+    helpContainer.style.display = '';
+    if(!visited) {
+        helpContainer.classList.remove('hide');
+        localStorage.setItem('visited', true);
+    }
+
+    helpContainer.style.transition = '0.5s ease-in-out';
+    const closeButton = createAndAppendElement(helpContainer, 'div', {
+        class: 'close',
+    });
+    closeButton.addEventListener('click', ()=>{
+        helpContainer.classList.add('hide');
+    });
+    help.addEventListener('click', ()=>{
+        helpContainer.classList.toggle('hide');
+    });
+    // ---
+
 });
